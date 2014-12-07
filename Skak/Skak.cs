bd577d7da31cd0e93add82e4bc4b0e50d15adbd9
Nekaerth <lasse.j.lundin@gmail.com>
2014-12-07 20:53:55 +0100

    Startede med at lave brikker

--- Skak/Skak.cs
+++ Skak/Skak.cs
@@ -6,5 +6,40 @@ using System.Threading.Tasks;
 
 namespace Skak {
 	public class Skak {
+		public Skak() {
+
+		}
+	}
+
+	public class Brik {
+
+		private BrikType type;
+		private Farve farve;
+		private int x;
+		private int y;
+
+		public Brik(BrikType type, Farve farve, int x, int y) {
+			this.type = type;
+			this.farve = farve;
+			this.x = x;
+			this.y = y;
+		}
+
+		public Boolean ILive() {
+			if (7 < x || x < 0 || 7 < y || y < 0) {
+				return false;
+			}
+			return true;
+		}
+
+
+	}
+
+	public enum BrikType {
+		Bonde, Tårn, Springer, Løber, Dronning, Konge
+	}
+
+	public enum Farve {
+		Hvid, Sort
 	}
 }
