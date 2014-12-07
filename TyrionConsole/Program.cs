3ee07a52b8cdb8331160f800fcc4b331d096b74c
Armienn <nigathr@gmail.com>
2014-12-07 13:44:13 +0100

    Added a console project

---
+++ TyrionConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TyrionConsole {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Daniel lugter af hest");
+			Console.ReadKey();
+		}
+	}
+}
